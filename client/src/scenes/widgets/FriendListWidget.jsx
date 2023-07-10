f6d69a57259a66f90308cc84c1a408da5b29aefd
Sindhura9686 <sindhurak.1si18ec098@gmail.com>
2023-07-10 16:59:36 +0530

    getFriends issue

--- client/src/scenes/widgets/FriendListWidget.jsx
+++ client/src/scenes/widgets/FriendListWidget.jsx
@@ -24,7 +24,18 @@ const FriendListWidget = ({ userId }) => {
   };
 
   useEffect(() => {
-    getFriends();
+    const getFriends = async () => {
+      const response = await fetch(
+        `http://localhost:3001/users/${userId}/friends`,
+        {
+          method: "GET",
+          headers: { Authorization: `Bearer ${token}` },
+        }
+      );
+      const data = await response.json();
+      dispatch(setFriends({ friends: data }));
+    };
+    // getFriends();
   }, []); 
   return (
     <WidgetWrapper>
